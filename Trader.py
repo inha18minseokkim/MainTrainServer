6678d9a1341d87913fc3b52630b1561fe5c5a4a9
bjm7701 <bjm7701@naver.com>
2022-07-14 11:04:26 +0900

    한국투자 API 계좌번호 변경 해서 key,secret값이 살짝 바뀌었습니다 시장가 매도/매수 기능 구현 완료했습니다

--- Trader.py
+++ Trader.py
@@ -4,7 +4,7 @@ import requests
 import json
 def getHash(apikey,secret,data): #api post 요청 시 사용 할 hash 함수
     #단독으로 사용하지 마세요. 세션이 없다는 조건을 guard 하지 않음
-    print(data["CANO"],data["ACNT_PRDT_CD"])
+    print('다음 정보를 hash',apikey,secret,data["CANO"],data["ACNT_PRDT_CD"])
     path = "uapi/hashkey"
     url = f"{Declaration.Base_URL}/{path}"
     headers = {
@@ -28,6 +28,7 @@ def buyMarketPrice(kakaoid,code,quantity): #현금 주식 매입 주문
         "ORD_QTY": f"{quantity}",  # 수량
         "ORD_UNPR": "0",  # 가격은 시장가이므로 비워놔
     }
+    h = getHash(cursession[DBManager.APIKEY],cursession[DBManager.SECRET],data)
     headers = {
         "content-type": 'application/json',
         "authorization": f"Bearer {cursession[DBManager.TOKEN]}",
@@ -35,19 +36,48 @@ def buyMarketPrice(kakaoid,code,quantity): #현금 주식 매입 주문
         "appSecret": cursession[DBManager.SECRET],
         "tr_id": "VTTC0802U",
         "custtype":"P",
-        "hashkey": getHash(cursession[DBManager.APIKEY],cursession[DBManager.SECRET],data)
+        "hashkey": h
     }
+    print(h)
     print(cursession[DBManager.CANO],cursession[DBManager.ACNT],data['CANO'],data['ACNT_PRDT_CD'])
     path = "/uapi/domestic-stock/v1/trading/order-cash"
     url = f"{Declaration.Base_URL}/{path}"
     res = requests.post(url, headers=headers, data = json.dumps(data)).json()
+    res['code'] = 1
     print(res)
     return res
 
-def sellMarketPrice(kakaoid,code,quantity):
+def sellMarketPrice(kakaoid,code,quantity): #현금 주식 매도 주문
     cursession = DBManager.getSessionInfo(kakaoid)
-    if cursession['code'] == 0:  # 현재 세션이 존재하지 않으면 0 리턴
-        return {'code': 0}
+    if cursession['code'] == 0: #현재 세션이 존재하지 않으면 0 리턴
+        return {'code' : 0}
+    #httprequest 부분 시작
+    data = {
+        "CANO": cursession[DBManager.CANO],
+        "ACNT_PRDT_CD": cursession[DBManager.ACNT],
+        "PDNO": f"{code}",  # 종목코드
+        "ORD_DVSN": "01",  # 시장가 코드
+        "ORD_QTY": f"{quantity}",  # 수량
+        "ORD_UNPR": "0",  # 가격은 시장가이므로 비워놔
+    }
+    h = getHash(cursession[DBManager.APIKEY],cursession[DBManager.SECRET],data)
+    headers = {
+        "content-type": 'application/json',
+        "authorization": f"Bearer {cursession[DBManager.TOKEN]}",
+        "appKey": cursession[DBManager.APIKEY],
+        "appSecret": cursession[DBManager.SECRET],
+        "tr_id": "VTTC0801U",
+        "custtype":"P",
+        "hashkey": h
+    }
+    print(h)
+    print(cursession[DBManager.CANO],cursession[DBManager.ACNT],data['CANO'],data['ACNT_PRDT_CD'])
+    path = "/uapi/domestic-stock/v1/trading/order-cash"
+    url = f"{Declaration.Base_URL}/{path}"
+    res = requests.post(url, headers=headers, data = json.dumps(data)).json()
+    res['code'] = 1
+    print(res)
+    return res
 
 
 
@@ -55,4 +85,4 @@ if __name__ == "__main__":
     Declaration.initiate()
     DBManager.sessionDBinitiate()
     DBManager.createDummyData()
-    print(buyMarketPrice('12181577','005930',1))
+    print(sellMarketPrice('12181577','005930',1))
