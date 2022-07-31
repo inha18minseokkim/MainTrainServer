a9ce4e66c4e4f17097f6e159e8ad80ddaafa685c
root <root@ip-172-31-1-68.us-west-1.compute.internal>
2022-07-31 21:22:14 +0900

    Fix main

--- routers/test.py
+++ routers/test.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/")
+@router.post("/")
 async def root():
     return {"message": "Hello World"}
 
-@router.get("/hello/{name}")
+@router.post("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
